adc294ca21920298039ca57510c37db3a3bd2abd
akramhany <akramhany65@gmail.com>
2024-06-26 20:28:19 +0300

    feat: connect to screw atlas database

--- server/app.js
+++ server/app.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import { config } from "dotenv";
+import { connectToCluster } from "./utils/database.js";
+
+config();
+console.log(process.env.DB_URI);
+
+connectToCluster(process.env.DB_URI)
+  .then((result) => {
+    console.log("Connection is done");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
